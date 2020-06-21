07f8719bbb723bbbb20b59a54790a74392959bfa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-21 08:12:41 +0000

    Deploy akayhu/js-vue-press to github.com/akayhu/js-vue-press.git:gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# js-vue-press
-javascript 整理
--- docs/.vuepress/components/Hello.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div style="color: red; font-size: 20px;">666 我是 vue</div>
-</template>
-
-<script>
-export default {
-  name: "Hello"
-}
-</script>
