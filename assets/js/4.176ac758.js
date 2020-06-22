a6bdeb780e6f58a47188aa453ea9440432595760
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-22 07:00:57 +0000

    Deploy akayhu/js-vue-press to github.com/akayhu/js-vue-press.git:gh-pages

---
+++ assets/js/4.176ac758.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{351:function(e,t,l){"use strict";l.r(t);var n={name:"Hello"},o=l(42),s=Object(o.a)(n,(function(){var e=this.$createElement;return(this._self._c||e)("div",{staticStyle:{color:"red","font-size":"20px"}},[this._v("Hello world")])}),[],!1,null,null,null);t.default=s.exports}}]);
